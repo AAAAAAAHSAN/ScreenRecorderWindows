dabe03564a3223e2286102686d3f6e415ec9f427
AAAAAAAHSAN <bangladeshahsan@gmail.com>
2021-04-14 12:42:27 +0600

    first of all, adding all the files

---
+++ ScreenRecorder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+using Accord.Video.FFMPEG;
+
+namespace ScreenRecorder
+{
+    class ScreenRecorder
+    {
+        //video variables
+        private Rectangle bounds;
+        private string outputPath = "";
+        private string tempPath = "";
+        private int fileCount = 1;
+        private List<string> inputImageSequence = new List<string>();
+
+        //file variables
+        private string audioName = "mic.wav";
+        private string videoName = "video.mp4";
+        private string finalName = "FinalVideo.mp4";
+
+        //time variables
+        Stopwatch stopwatch = new Stopwatch();
+
+        //audio variables
+        public static class NativeMethods
+        {
+            [DllImport("winmm.dll", EntryPoint = "mciSendStringA", CharSet = CharSet.Ansi, SetLastError = true, ExactSpelling = true)]
+            private static extern int mciSendString(string lpstrCommand, string lpstrReturnString, int uReturnLength, int hwndCallback);
+
+        }
+    }
+}
